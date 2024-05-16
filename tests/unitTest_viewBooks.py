37eb1b386a78d52658756038c6ce157dd8191f17
Patrice Remy Lou D Virata <trish.virata@gmail.com>
2024-05-16 00:46:25 -0500

    tests

---
+++ tests/unitTest_viewBooks.py
@@ -0,0 +1,46 @@
+# Selenium test script
+# test script to verify a valid user is logged out successfully
+import unittest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+
+import warnings
+
+
+class ll_ATS(unittest.TestCase):
+    # set up the test class - assign the driver to Chrome - if using a different
+    # browser, change the browser name below
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        warnings.simplefilter('ignore', ResourceWarning)  # ignore resource warning if occurs
+
+    # If login is successful, 'Logout' will be displayed as the text in the Navbar
+    def test_ll(self):
+        user = "pvirata"  # must be a valid username for the application
+        pwd = "cookies99!"  # must be the password for a valid user
+        name = "cookies"
+
+        driver = self.driver
+        driver.maximize_window()
+        driver.get("https://pvirata.pythonanywhere.com")
+
+        elem = driver.find_element(By.ID, "id_username")
+        elem.send_keys(user)
+        elem = driver.find_element(By.ID, "id_password")
+        elem.send_keys(pwd)
+        time.sleep(3)
+        elem.send_keys(Keys.RETURN)
+        driver.get("https://pvirata.pythonanywhere.com")
+        time.sleep(3)
+        driver.find_element(By.XPATH, "//a[contains(., 'Books')]").click()
+        time.sleep(3)
+
+        def tearDown(self):
+            self.driver.quit()
+
+
+if __name__ == "__main__":
+    unittest.main(warnings='ignore')
